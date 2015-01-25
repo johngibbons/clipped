e76e98e23315b234aef9df933a5e2da5f2a83eca
John Gibbons <johngibbons10@gmail.com>
2015-01-25 13:28:27 -0700

    add top nav and modify routes for linking

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/about'
-
-  get 'static_pages/help'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   resources :entourage_items
 
