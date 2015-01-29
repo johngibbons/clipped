4464866d19906505e2670fd262acb33e1d47f601
John Gibbons <johngibbons10@gmail.com>
2015-01-29 13:53:08 -0700

    Add minitest, backtrace silencer, guard for testing
    Add tools as described in railstutorial.org ch 3 exercises for advanced
    testing.  Allows for green and red display when testing, silences long
    traces that can occur with RVM, and allows for automatic testing upon
    changes using Guard.

--- Gemfile
+++ Gemfile
@@ -50,6 +50,12 @@ group :development, :test do
 
 end
 
+group :test do
+  gem 'minitest-reporters', '1.0.5'
+  gem 'mini_backtrace',     '0.1.3'
+  gem 'guard-minitest',     '2.3.1'
+end
+
 gem 'rails_12factor', group: :production
 
 ruby "2.2.0"
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Clipped"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Clipped"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Clipped"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Clipped"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
