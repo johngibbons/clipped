a1d54f317b639b67c67120b4f7376279504d1ba6
John Gibbons <johngibbons10@gmail.com>
2015-01-29 19:07:58 -0700

    Create User model with Name and Email fields
    Start of the creation of a User model to authenticate users of the app.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Clipped"
+  end
+
+
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "title", full_title("Sign up")
   end
 
 end
