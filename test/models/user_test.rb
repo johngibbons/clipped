17627c5444216f2871cd496309b1f6a6212894a5
John Gibbons <johngibbons10@gmail.com>
2015-01-29 20:05:46 -0700

    Add email uniqueness validation and downcasing
    Add validation to User model for uniqueness as well as index on database
    for enforcing uniqueness and efficiency in getting user when logging in, etc.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,4 +43,17 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be downcased" do
+    mixed_case_email = "SaMple@eXaMplE.com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal @user.reload.email, mixed_case_email.downcase
+  end
 end
