6870dd9f8d2726d2b8e1adc130efcb43d3d17973
John Gibbons <johngibbons10@gmail.com>
2015-01-31 18:01:09 -0700

    Set admin to be the only ones who can delete users and update tests
    Make sure that delete links only show up for admins on user index page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   def new
     @user = User.new
   end
@@ -38,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -45,6 +52,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+    #determines whether user is logged in and if not directs them to login page
     def logged_in_user
       unless logged_in?
         store_location
@@ -53,8 +61,14 @@ class UsersController < ApplicationController
       end
     end
 
+    #confirms that user is the current_user
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -55,4 +55,20 @@ class UsersControllerTest < ActionController::TestCase
     assert_not @other_user.reload.admin?
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in but not admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert flash.empty?
+    assert_redirected_to root_url  
+  end
 end
