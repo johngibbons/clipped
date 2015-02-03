a6a18fde834746d5ad3f358b2a46e3fcfd545093
John Gibbons <johngibbons10@gmail.com>
2015-02-03 12:21:16 -0700

    Add password reset email functionality
    Allow users to reset passwords by asking for their registerd email and
    then sending a link via that email to a password edit page.  They then
    can enter a new password and upon doing so are logged in.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202051044) do
+ActiveRecord::Schema.define(version: 20150203182909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20150202051044) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
