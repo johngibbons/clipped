d815a681fd14c7feab94c297bcaac0d105bbfee6
John Gibbons <johngibbons10@gmail.com>
2015-03-06 16:57:25 -0700

    Working tag display on uploader
    Tags display proper formatting on uploader by instantiating a new
    formatTag function on each upload.

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,53 +1,6 @@
 $(document).ready(function () {
 
-  deleteNextIteration = true;
-
-  function formatTags(tag_input_field) {
-    $(tag_input_field).on('focusout',function(){    
-      var txt= this.value.replace(/[^a-zA-Z0-9\+\-\.\#\s]/g,''); // allowed characters
-      if(txt) {
-        $(this).before('<span class="temporary tag"><span class="tag-data">'+ txt.toLowerCase() + '</span>' + '<span class="delete-tag fa fa-remove"></span></span>');
-      }
-      this.value="";
-      }).on('keyup',function( e ){
-      // if: comma,enter (delimit more keyCodes with | pipe)
-      if(/^188$/.test(e.which)) {
-        if(e.keyCode == 13) {
-          e.preventDefault;
-          return false;
-        }
-        $(this).focusout(); 
-      }
-
-      else if((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-        $('button[type=submit] .default').click();
-        return false;
-      }
-      else if(/^8$/.test(e.which)) {
-        if(deleteNextIteration) {
-          $('.tag').last().remove();
-        }
-      }
-
-      if($(this).val().length == 0 ) {
-        deleteNextIteration = true;
-      }
-      else {
-        deleteNextIteration = false;
-      }
-    });
-  };
-
-  function getTags( tags ) {
-    var a = [];
-    for ( var i = 0; i < tags.length; i++ ) {
-      a.push( tags[ i ].textContent );
-    }
-    return a.join();
-  }
-
-  formatTags("upload_tag_list");
-
+  formatTags("#upload_tag_list");
 
   $('#update-tags-btn').click(function() {
     $('#update-tags-form').submit();
