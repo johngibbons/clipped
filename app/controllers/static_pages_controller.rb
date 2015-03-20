6401d76570b4ab81b138d83bb34ba43a76e9e3d6
John Gibbons <johngibbons10@gmail.com>
2015-03-19 18:02:07 -0600

    Home page mobile styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     @uploads = StaticPagesPolicy::Scope.new(current_user, Upload).resolve
     @most_popular = @uploads.sorted_by_weighted_score
-
-    @most_used_tags = ActsAsTaggableOn::Tag.most_used(30)
+    @most_used_tags = Upload.where(approved: true).tag_counts.order(taggings_count: :desc).limit(30)
   end
 
   def help
