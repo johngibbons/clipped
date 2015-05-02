c9ac7236624d449bce444013ae51bcbb665adbae
John Gibbons <johngibbons10@gmail.com>
2015-05-01 19:19:21 -0600

    Fix Facebook omniauth login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'], image_size: { width: 300 }
+  provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'], { image_size: { width: 300 }, secure_image_url: true }
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
       :image_size => 300
