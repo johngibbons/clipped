d4cb372918d9616382afc8b80b34ebb7e2eeca6a
John Gibbons <johngibbons10@gmail.com>
2015-08-26 09:06:17 -0400

    work on mailer previews

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ApplicationMailer
+  before_action :add_inline_attachments!
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -6,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Clipped account activation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,4 +19,10 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: "Password reset"
   end
+
+  private
+
+    def add_inline_attachments!
+      attachments.inline["logo-header.png"] = File.read("assets/images/logo-header.png")
+    end
 end
