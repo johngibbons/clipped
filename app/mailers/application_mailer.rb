288548b61f35a5c6be636ac915bad11406a817d0
John Gibbons <johngibbons10@gmail.com>
2016-05-08 13:14:48 -0700

    email logo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@clipped.io"
-
-  def mailer
-    attachments['logo-header.png'] = File.read('/logo-header.png')
-  end
+  layout 'mailer'
 end
